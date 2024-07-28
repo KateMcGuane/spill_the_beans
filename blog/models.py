838dfb68dd6ca14f6fdea7ca4d34eed9859c7435
Kate Mcguane <katemcguane@hotmail.com>
2024-07-28 13:18:37 +0000

    Add notes to models.py

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Draft"), (1, "Published"))  # puts limit on status; human-readable vs integers
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)  # generates single line form
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()  # multi-line textarea input
+    created_on = models.DateTimeField(auto_now_add=True)  # computer's time on save
     status = models.IntegerField(choices=STATUS, default=0)
