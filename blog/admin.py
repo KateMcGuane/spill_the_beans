28b22e6fb94083cb173a7dd4efa0c8d1c519b4a0
Kate Mcguane <katemcguane@hotmail.com>
2024-07-28 15:47:25 +0000

    Add notes to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post  # . indicating directory of models(same as admin.py)
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post)  # allows custom models to appear; import Post model
