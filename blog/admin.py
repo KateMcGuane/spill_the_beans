87496c88b2310503b652cdd408bb54ad2f0fb198
Kate Mcguane <katemcguane@hotmail.com>
2024-07-28 21:23:46 +0000

    Run migrations & update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Post  # . indicating directory of models(same as admin.py)
+from .models import Comment
+
 
 # Register your models here.
 admin.site.register(Post)  # allows custom models to appear; import Post model
+admin.site.register(Comment)
