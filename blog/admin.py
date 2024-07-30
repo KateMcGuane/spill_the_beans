c747fc4644b462c4eaf9f7193ba9ff156ff41e7b
Kate Mcguane <katemcguane@hotmail.com>
2024-07-30 17:06:33 +0000

    Update blog app to use summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Post, Comment  # . indicating directory of models (same as admin.py); Can insert multiple models
+from django_summernote.admin import SummernoteModelAdmin
+
+
+#  Registering a class with a decorator is more Pythonic & allows us to 
+#  customise how the models we are registering will appear on the admin site.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 # Register your models here.
-admin.site.register(Post)  # allows custom models to appear; import Post model
 admin.site.register(Comment)
