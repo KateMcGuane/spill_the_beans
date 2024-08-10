ef684d92ffacd9f26b0f3a78ab168bec32138acf
Kate Mcguane <katemcguane@hotmail.com>
2024-08-10 20:37:02 +0000

    Ensure there are no disparities between source code & project development

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,14 @@ from .models import Post
 
 
 # Create your views here.
-
+# Class-based view
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
 
-
+# Function-based view
 def post_detail(request, slug):
 
     """
