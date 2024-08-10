7fb6f549203848a4870d0dfa288bad40325a0410
Kate Mcguane <katemcguane@hotmail.com>
2024-08-10 20:51:50 +0000

    Add notes to views.py

--- blog/views.py
+++ blog/views.py
@@ -27,8 +27,12 @@ def post_detail(request, slug):
     """
 
     queryset = Post.objects.filter(status=1)
+    # Helper function get_object_or_404
+    # Get data or raise a Http404 error if data object does not exist
+    # The variable assigned to the result
     post = get_object_or_404(queryset, slug=slug)
 
+    # Helper function render() returns HttpResponse object
     return render(
         request,
         "blog/post_detail.html",
