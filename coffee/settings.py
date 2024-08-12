4f1b173357bee253db37a1e66f3dfafe8345e6f7
Kate Mcguane <katemcguane@hotmail.com>
2024-08-12 21:51:13 +0000

    Create the About app

--- coffee/settings.py
+++ coffee/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-katemcguane-spillthebea-gkfuotnswlz.ws.codeinstitute-ide.net",
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
