d338397ddda666afb270a9ae45cf3972d742d33f
Kate Mcguane <katemcguane@hotmail.com>
2024-08-12 21:00:46 +0000

    Revert "Establish admin for About; Import model & SummernoteModelAdmin"
    This reverts commit 35d6abcee42e2efdf21d19083a1c4155d3a87fd1.
    
    Need to wipe in order to fix bad migration

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 #  Registering a class with a decorator is more Pythonic & allows us to 
-#  Customise how the models we are registering will appear on the admin site.
+#  customise how the models we are registering will appear on the admin site.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
