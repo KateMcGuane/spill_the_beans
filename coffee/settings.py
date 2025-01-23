8d643a59392f43105b4d5d869fbd28491b18537f
Kate Mcguane <katemcguane@hotmail.com>
2025-01-23 18:09:16 +0000

    Set up env & dependencies after creating new workspace

--- coffee/settings.py
+++ coffee/settings.py
@@ -28,10 +28,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "8000-katemcguane-spillthebea-gkfuotnswlz.ws.codeinstitute-ide.net",
+    "8000-katemcguane-spillthebea-mt7uebshs3c.ws.codeinstitute-ide.net",
     ".herokuapp.com"
 ]
 
