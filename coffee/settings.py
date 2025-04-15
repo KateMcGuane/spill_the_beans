1adeea38779740df8b5eb18cdb1f168908900494
KateMcGuane <katemcguane@hotmail.com>
2025-04-15 15:58:03 +0100

    Change allowed hosts (Codeanywhere to VSC)

--- coffee/settings.py
+++ coffee/settings.py
@@ -29,10 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    "8000-katemcguane-spillthebea-mt7uebshs3c.ws.codeinstitute-ide.net",
+    "127.0.0.1",
     ".herokuapp.com"
 ]
 
