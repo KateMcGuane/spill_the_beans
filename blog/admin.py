12d032caab648ff5ad2adcc10c4843ce891a3c8d
KateMcGuane <katemcguane@hotmail.com>
2025-04-20 17:39:58 +0100

    Fix typos

--- blog/admin.py
+++ blog/admin.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
-from .models import (  # . indicating directory of models (same as admin.py)
-    Post,  # Importing the Post model from models.py
-    Comment,  # Importing the Comment model from models.py
-)
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 #  Registering a class with a decorator is more Pythonic & allows us to
-#  customise how the models we are registering will appear on the admin site.
+#  Customise how the models we are registering will appear on the admin site.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on',)
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    list_filter = ('status', 'created_on',)
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
