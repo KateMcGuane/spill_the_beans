553c1944713ed48e87a9c05fb74242807c334da8
KateMcGuane <katemcguane@hotmail.com>
2025-04-21 23:02:36 +0100

    Add docstrings to about views

--- about/views.py
+++ about/views.py
@@ -7,7 +7,23 @@ from .forms import CollaborateForm
 
 def about_me(request):
     """
-    Renders the About page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Retrieves the most recent instance of :model:`about.About` and
+    handles form submissions for collaboration request. Upon a succesful
+    form submission, the request is saved and a success message is shown.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`,
+        either pre-populated or empty.
+
+    **Template**
+    :template:`about/about.html`
+        Renders both the about information & collboration form.
     """
     about = About.objects.all().order_by('-updated_on').first()
     if request.method == "POST":
