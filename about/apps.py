2aeec91f4307a0f409dcc0e364de43bdf97c46af
KateMcGuane <katemcguane@hotmail.com>
2025-04-22 20:03:58 +0100

    Personalise footer

--- about/apps.py
+++ about/apps.py
@@ -3,7 +3,10 @@ from django.apps import AppConfig
 
 class AboutConfig(AppConfig):
     """
-    Provides primary key type for about app
+    Sets up the settings for the 'about' app.
+
+    This class sets the default primary key field type & 
+    registers the app with the Django project.
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'about'
