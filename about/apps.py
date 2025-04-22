564bbae9563f1d585fc37993a4825c61d6ea8ad3
KateMcGuane <katemcguane@hotmail.com>
2025-04-22 20:07:14 +0100

    Write docstrings for apps

--- about/apps.py
+++ about/apps.py
@@ -5,7 +5,7 @@ class AboutConfig(AppConfig):
     """
     Sets up the settings for the 'about' app.
 
-    This class sets the default primary key field type & 
+    This class sets the default primary key field type &
     registers the app with the Django project.
     """
     default_auto_field = 'django.db.models.BigAutoField'
