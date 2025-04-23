2b7530092ea6713f91bb688f75f3e7d5040322fd
KateMcGuane <katemcguane@hotmail.com>
2025-04-23 07:42:33 +0100

    Update CSS

--- terms/admin.py
+++ terms/admin.py
@@ -5,4 +5,4 @@ from .models import TermsOfUse
 # Register your models here.
 @admin.register(TermsOfUse)
 class TermsOfUseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at',)
+    list_display = ('title', 'updated_on',)
