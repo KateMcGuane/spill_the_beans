2bc10a07fa4099b09301b6086fd95398eb09d061
KateMcGuane <katemcguane@hotmail.com>
2025-04-23 07:13:03 +0100

    Create terms of use html

--- terms/views.py
+++ terms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import TermsOfUse
+
 
 # Create your views here.
+def terms_of_use(request):
+    terms = TermsOfUse.objects.all().order_by('-updated_on').first()
+    return render(request, 'terms/terms_of_use.html', {'terms': terms})
