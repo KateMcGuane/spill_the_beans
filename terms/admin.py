676a5342d7803d5c45d16975b90b5a9bcb16fd9f
KateMcGuane <katemcguane@hotmail.com>
2025-04-28 22:40:08 +0100

    Change admin type for terms>admin.py

--- terms/admin.py
+++ terms/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import TermsOfUse
 
 
 # Register your models here.
 @admin.register(TermsOfUse)
-class TermsOfUseAdmin(admin.ModelAdmin):
+class TermsOfUseAdmin(SummernoteModelAdmin):
     list_display = ('title', 'updated_on',)
+    readonly_fields = ('updated_on',)
+    summernote_fields = ('content',)
